6e57e11449388beb5f44c4d5edae63ef17ddbdf5
nicolas <ndifabio92>
2020-10-26 01:03:14 -0300

    comienzo de la creacion del servidor en node con express

---
+++ Fernando/10-calendar-backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+// Crear el servidor de express
+const app = express();
+
+
+// Rutas
+app.get('/', ( req, res ) => {
+    console.log('se requiere el /');
+    res.json({
+        ok: true
+    })
+})
+
+
+// Escuchar peticiones
+app.listen( 4000, () => {
+    console.log('Servidor corriendo en puerto');
+})
