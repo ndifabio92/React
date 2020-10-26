8211486a3e4ace440cdf1aa59f2ec956e5ea5001
nicolas <ndifabio92>
2020-10-26 16:31:14 -0300

    se genera conexion a base de datos, auth, endpoints

--- Fernando/10-calendar-backend/index.js
+++ Fernando/10-calendar-backend/index.js
@@ -1,19 +1,33 @@
 const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('./database/config');
+require('dotenv').config();
+
+// console.log( process.env );
 
 // Crear el servidor de express
 const app = express();
 
+// Base de datos
+dbConnection();
+
+// CORS
+app.use(cors());
+
+// Directorio Publico
+app.use( express.static('public') );
+
+// Lectura y parseo del body
+app.use( express.json() );
 
 // Rutas
-app.get('/', ( req, res ) => {
-    console.log('se requiere el /');
-    res.json({
-        ok: true
-    })
-})
+// TODO: auth // crear, login, renw
+app.use('/api/auth', require('./routes/auth') );
+
+// TODO: CRUD: Eventos
 
 
 // Escuchar peticiones
-app.listen( 4000, () => {
-    console.log('Servidor corriendo en puerto');
+app.listen( process.env.PORT , () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
 })
