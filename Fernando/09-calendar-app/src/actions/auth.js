f473b57fcee071711da262f29692f47d9e147423
nicolas <ndifabio92>
2020-11-01 19:12:57 -0300

    .env

---
+++ Fernando/09-calendar-app/src/actions/auth.js
@@ -0,0 +1,68 @@
+import Swal from "sweetalert2";
+import { fetchConToken, fetchSinToken } from "../helpers/fetch"
+import { types } from "../types/types";
+
+export const startLogin = ( email, password ) => {
+    return async ( dispatch ) => {
+        
+        const resp = await fetchSinToken( 'auth', { email, password }, 'POST');
+        const { ok, token, uid, name, msg } = await resp.json();
+
+        if( ok ) {
+            localStorage.setItem('token', token );
+            localStorage.setItem('token-init-date', new Date().getTime() );
+            dispatch( login({
+                uid,
+                name 
+            }));
+        } else {
+            Swal.fire('Error', msg, 'error' );
+        }
+    }
+}
+
+export const startRegister = ( email, password, name ) => {
+    return async ( dispatch ) => {
+
+        const resp = await fetchSinToken( 'auth/new', { email, password, name }, 'POST');
+        const { ok, token, uid, name:displayName, msg } = await resp.json();
+
+        if( ok ) {
+            localStorage.setItem('token', token );
+            localStorage.setItem('token-init-date', new Date().getTime() );
+            dispatch( login({
+                uid,
+                name: displayName
+            }));
+        } else {
+            Swal.fire('Error', msg, 'error' );
+        }
+    }
+}
+
+export const startChecking = () => {
+    return async ( dispatch ) => {
+
+        const resp = await fetchConToken( 'auth/renew');
+        const { ok, token, uid, name:displayName, msg } = await resp.json();
+
+        if( ok ) {
+            localStorage.setItem('token', token );
+            localStorage.setItem('token-init-date', new Date().getTime() );
+            dispatch( login({
+                uid,
+                name: displayName
+            }));
+        } else {
+            Swal.fire('Error', msg, 'error' );
+            dispatch( checkingFinish() );
+        }
+    }
+}
+
+const checkingFinish = () => ({ type: types.authCheckingFinish })
+
+const login = ( user ) => ({
+    type: types.authLogin,
+    payload: user
+})
--- Fernando/09-calendar-app/src/types/types.js
+++ Fernando/09-calendar-app/src/types/types.js
@@ -6,5 +6,12 @@ export const types = {
     eventSetActive: '[EVENT] Set Active',
     eventClearActiveEnvent: '[EVENT] Clear Active',
     eventUpdated: '[EVENT] Event Updated',
-    eventDeleted: '[EVENT] Event Deleted'
+    eventDeleted: '[EVENT] Event Deleted',
+
+    authCheckingFinish: '[AUTH] Finish Cheking Login State',
+    authStartLogin: '[AUTH] Start Login',
+    authLogin: '[AUTH] Login',
+    authStartRegister: '[AUTH] Start Register',
+    authStartRegisterRenew: '[AUTH] Start Register Renew',
+    authLogout: '[AUTH] Logout'
 }
