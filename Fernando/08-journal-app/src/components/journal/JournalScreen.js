fa8a9778ad7de098713be1b8117496c134fff1a5
nicolas <ndifabio92>
2020-11-09 19:23:11 -0300

    curso fernando

--- Fernando/08-journal-app/src/components/journal/JournalScreen.js
+++ Fernando/08-journal-app/src/components/journal/JournalScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Sidebar } from './Sidebar';
 import { NoteScreen } from '../notes/NoteScreen';
-// import { NothingSelected } from './NothingSelected';
+import { useSelector } from 'react-redux';
+import { NothingSelected } from './NothingSelected';
 
 
 export const JournalScreen = () => {
+
+    const { active } = useSelector( state => state.notes );
     return (
         <div className="journal__main-content">
             
@@ -12,10 +15,9 @@ export const JournalScreen = () => {
 
 
             <main>
-
-                {/* <NothingSelected /> */}
-                <NoteScreen />
-
+                {
+                    ( active ) ? ( <NoteScreen/> ) : ( <NothingSelected/> )
+                }
             </main>
 
 
---
+++ Fernando/08-journal-app/src/reducers/notesReducer.js
@@ -0,0 +1,30 @@
+/*
+    {
+        notes: [],
+        active: null,
+        active: {
+            id: 'asdada',
+            title: 'asdada',
+            body: 'asdasda',
+            imageUrl: 'asda',
+            date: 1234
+        }
+    }
+*/
+
+const initialState = {
+    notes: [],
+    active: null,
+}
+
+export const notesReducer = ( state = initialState, action ) => {
+
+    const hola = [];
+    let p = {};
+    hola.map( )
+    switch ( action.type ) {
+    
+        default:
+            return state;
+    }
+}
