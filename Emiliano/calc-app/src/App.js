9e622c673a575177116197c3ca8f689f9f29c2e2
ndifabio <nicolasdifabio92@gmail.com>
2020-12-06 23:59:17 -0300

    new curse

---
+++ Emiliano/calc-app/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+
+export const App = () => {
+    return (
+        <main className = "react-calculator">
+            <div className="result">
+            </div>
+            <div className="numbers">
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button>0</button>
+            </div>
+            <div className="functions">
+                <button> Clear </button>
+                <button> Remove </button>
+            </div>
+            <div className="math-operations">
+                <button>+</button>
+                <button>-</button>
+                <button>*</button>
+                <button>/</button>
+                <button>=</button>
+            </div>
+        </main>
+    )
+}
+
+export default App
